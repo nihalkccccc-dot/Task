a5725abe3507a6d7b5d929b125b127ec3b63710e
nihalkccccc-dot <nihalkccccc@gmail.com>
2026-06-04 12:03:04 -0400

    Task commit

--- frontend/src/StudyLoginPage.jsx
+++ frontend/src/StudyLoginPage.jsx
@@ -1,55 +1,56 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
-function StudyLoginPage(props) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [isRegistering, setIsRegistering] = useState(false);
-    
+function StudyLoginPage(props){
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+
     const handleLogin = async () => {
         try {
-            const response = await axios.post('https://task-bupu.onrender.com/login', { username, password });
+            const response = await axios.post("https://task-bupu.onrender.com/login", { username, password });
             if (response.status === 200) props.setIsLoggedIn(true);
         } catch (error) {
-            alert("Invalid username or password");
+            alert("Invalid username or password.");
+
         }
     };
-    
+
     const handleRegister = async () => {
         try {
-            const response = await axios.post('https://task-bupu.onrender.com/register', { username, password });
+            const response = await axios.post("https://task-bupu.onrender.com/register", { username, password });
             if (response.status === 201) {
-                alert("Account created! log in now.");
+                alert("Account created! Log in now.");
                 setIsRegistering(false);
             }
         } catch (error) {
-            alert("Registration failed");
+            alert("Registration failed.");
         }
     };
 
     return (
-        <div className="d-flex justify-content-center align-items-center bg-light min-vh-100">           
-           <div className="bg-white p-5 shadow rounded border-primary border" style={{ width: '400px' }}>
-           <h2 className="text-center mb-4 text-primary fw-bold">
-            {isRegistering ? "Create Account" : "Planner Login"}
+        <div className="d-flex justify-content-center align-items-center bg-light min-vh-100">
+        <div className="bg-white p-5 shadow rounded border-top border-primary border-5" style={{ width: '400px' }}>
+            <h2 className ="text-center mb-4 text-primary fw-bold">
+                {isRegistering ? "Create Account" : "PlannerLogin"}
             </h2>
-            <input type="text" placeholder="Username" className="form-control mb-3"  onChange={(e) => setUsername(e.target.value)} />
-            <input type="password" placeholder="Password" className="form-control mb-3" onChange={(e) => setPassword(e.target.value)} />
-
-            {isRegistering ? (
-            <button className="btn btn-success w-100 mb-3" onClick={handleRegister}>Sign Up</button>
-            ) : (
-            <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
-            )}
-
-            <div className="text-center mt-3">
-            <span className="text-primary text-decoration-underline" style={{ cursor: 'pointer' }} onClick={() => setIsRegistering(!isRegistering)}>
+            <input type="text" placeholder="Username" className="from-control mb-4" onClick={(e) => setUsername(e.target.value)}/>
+            <input type="password" placeholder="Password" className="from-control mb-4" onChange={(e) => setPassword(e.target.value)}/>
+         
+         {isRegistering ? (
+         <button className="btn btn-success w-100 mb-3" onClick={handleRegister}>Sign Up</button>
+    ) : (
+        <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
+    )}
+    <div className="text-center mt-3">
+        <span className="text-primary text-decoration-underline" style={{ cursor: 'pointer' }} onClick={() => setIsRegistering(!isRegistering)}>
                 {isRegistering ? "Already have an account? Log in" : "Don't have an account? Register"}
             </span>
-            </div>
         </div>
     </div>
-    )
-}
+</div>
+    
 
+)
+}
 export default StudyLoginPage;
--- frontend/src/TaskCard.jsx
+++ frontend/src/TaskCard.jsx
@@ -2,24 +2,32 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function TaskCard(props) {
+    console.log(props.task);
     return (
-        <Card style={{ width: '18rem' }}
-        className={'shadow-sm mb-3 ${props.isDarkMode ? "bg-secondary text-dark border-dark" : "bg-white border-primary"}'}
-       >
-       <Card.Header className={'fw-bold ${props.isDarkMode ? "bg-light text-dark" : "bg-primary text-white"}'}>
-        {props.subject}
-        </Card.Header>
-        <Card.Body>
-            <Card.Title className={props.isDarkMode ? "text-warning fs-6" : "text-danger fs-6"}>
-            Due: {props.deadline}
-            </Card.Title>
-            <Card.Text>{props.taskDetails}</Card.Text>
-            
-            <Button variant="success" className="w-100" onClick={props.deleteTask}>
-                Mark as Done ✔               
-            </Button>
-        </Card.Body>
+        <Card
+        style={{ width: '18rem'}}
+        className={'shadow-sm mb-3 ${props.isDarkMode ? "bg-secondary text-dark border-light" : "bg-white border "}'}>
+            <Card.Header className={`fw-bold ${
+  props.isDarkMode
+    ? "bg-dark text-light"
+    : "bg-primary text-white"
+}`}>
+                {props.task.title}
+            </Card.Header>
+            <Card.Body>
+                <Card.Title className={props.isDarkMode ? "text-warning fs-6" : "text-danger fs-6"}>
+                  Due:  {props.task.dueDate}
+                </Card.Title>
+                <Card.Text>
+                    {props.task.description}
+                </Card.Text>
+
+                <Button variant="success" className="w-100" onClick={props.deleteTask}>
+                    Mark as Done
+                </Button>
+            </Card.Body>
         </Card>
     );
 }
+
 export default TaskCard;
